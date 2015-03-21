a28ba3fd223cbf16d625795042dd8e0878517973
Askar Akhmerov <askar.akhmerov@smava.de>
2015-03-21 15:45:41 +0100

    add nginx config

--- src/main/webapp/js/views/landing/LandingView.js
+++ src/main/webapp/js/views/landing/LandingView.js
@@ -12,15 +12,31 @@ define([
 
     var LandingView = SimpleView.extend({
 
-        my_client_id : '3bfc971fe4a14065a3f68c0bb0e6d040', // Your client id
+        CLIENT_ID : '3bfc971fe4a14065a3f68c0bb0e6d040', // Your client id
         my_secret : '2030d51fcb844d9e8dceb3a13f4ec9c9', // Your secret
-        redirect_uri : 'http://localhost:9000/#ride', // Your redirect uri
+        REDIRECT_URI : 'http://localhost:9000/#ride', // Your redirect uri
         scopes : 'user-read-private user-read-email',
+
+        getLoginURL : function (scopes) {
+            return 'https://accounts.spotify.com/authorize?client_id=' + this.CLIENT_ID +
+            '&redirect_uri=' + encodeURIComponent(this.REDIRECT_URI) +
+            '&scope=' + encodeURIComponent(scopes.join(' ')) +
+            '&response_type=code';
+        },
+
+        getUserData: function (accessToken) {
+            return $.ajax({
+                url: 'https://api.spotify.com/v1/me',
+                headers: {
+                    'Authorization': 'Bearer ' + accessToken
+                }
+            });
+        },
 
         template: Handlebars.compile(landingTemplate),
 
         events: {
-
+            'click .btn':'handleClick'
         },
 
         filters: {
@@ -36,11 +52,17 @@ define([
         initialize: function (options) {
             this.options = $.extend({}, options);
             this.options.params = this.parseUrlParams(this.options.params);
-            _.bindAll(this, 'render');
+            _.bindAll(this, 'render','getLoginURL','getUserData','login');
             this.elements = new Elements();
             this.render();
         },
 
+        login : function () {
+            var url = this.getLoginURL([
+                'user-read-email'
+            ]);
+            var w = window.location = url;
+        },
 
         render: function () {
             this.$el.html(this.template({}));
--- src/main/webapp/js/views/ride/RideView.js
+++ src/main/webapp/js/views/ride/RideView.js
@@ -13,7 +13,12 @@ define([
     'handlebars'
 ], function ($, _, Backbone, SimpleView, Elements, landingTemplate, Handlebars) {
 
-    var LandingView = SimpleView.extend({
+    var RideView = SimpleView.extend({
+        CLIENT_ID : '3bfc971fe4a14065a3f68c0bb0e6d040', // Your client id
+        my_secret : '2030d51fcb844d9e8dceb3a13f4ec9c9', // Your secret
+        REDIRECT_URI : 'http://localhost:9000/#ride', // Your redirect uri
+        scopes : 'user-read-private user-read-email',
+        TOKEN_URL : '/api/token',
 
         template: Handlebars.compile(landingTemplate),
 
@@ -34,10 +39,32 @@ define([
         initialize: function (options) {
             this.options = $.extend({}, options);
             _.bindAll(this, 'render');
-            this.elements = new Elements();
+            this.code = window.location.href.split('?')[1].split('=')[1].split('#')[0];
+
+            $.ajax({
+                url: this.TOKEN_URL,
+                headers: {
+                    "Accept" : "application/json; charset=utf-8",
+                    'Authorization': 'Basic ' + window.btoa(this.CLIENT_ID + ":" + this.my_secret)
+                },
+                type: 'POST',
+                data: {
+                    'grant_type': 'authorization_code',
+                    'code' : this.code,
+                    'redirect_uri' : this.REDIRECT_URI
+                },
+                success: function(data, textStatus, jqXHR){
+                    console.log(data);
+                    //data - response from server
+                }
+            });
+
             this.render();
         },
 
+        getUserInfo : function (response) {
+            console.log(response);
+        },
 
         render: function () {
             this.$el.html(this.template({}));
@@ -45,5 +72,5 @@ define([
         }
     });
 
-    return LandingView;
+    return RideView;
 });
